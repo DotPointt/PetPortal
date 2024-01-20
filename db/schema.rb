59e9efb8baa939d32730f1a4f97565e48197735e
DotPointt <daukaevk@gmail.com>
2024-01-21 00:07:48 +0300

    db:migrate >> schema changed to 'enable_extension plpgsql'

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2024_01_20_141922) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.text "title"
     t.text "description"
