f87f5e964708822f6599d750173f903d3339ad84
Timur <timur.minullin535@gmail.com>
2024-01-24 01:23:40 +0300

    add conrollers and roures

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,42 @@
 class ProjectsController < ApplicationController
-    def index    
-    end
+  def index; end
 
+  def new 
+    @project = Project.new
+  end
+
+  def edit; end 
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path, notice: "Created Successful"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path, notice: "Update Successful"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @project.destroy
+    redirect_to projects_path, notice: "Project destroyed"
+  end
+
+  private
+
+  def set_project
+    @project = Project.find_by(id: params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
