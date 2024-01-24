88474a4c6cb0e8c7b6c27977b3ba71b1c77a27d6
DotPointt <daukaevk@gmail.com>
2024-01-25 02:55:51 +0300

    added pagination, added .log to gitignore

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.7.4'
 
 gem 'pg'
 
+gem 'kaminari'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.7', '>= 6.1.7.6'
 # Use sqlite3 as the database for Active Record
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show edit update destroy]
 
   def index;
-    @projects = Project.all
+    @projects = Project.order(params[:sort]).page(params[:page]).per(5)
   end
 
   def new 
